53d956f1661882a49fcd3a819c2c149432dd7fc6
Cesar9810 <cesarcalderonorozco@gmail.com>
2024-05-29 21:19:50 -0500

    project-sally

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { ECommersApp } from './ECommersApp' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <ECommersApp />
+  </React.StrictMode>
+)
